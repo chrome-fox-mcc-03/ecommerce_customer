573d264b1e1282409df66b725ec01e76d97881bf
Ridza Adhandra <andradhandra@gmail.com>
2020-03-27 00:00:09 +0700

    After deploy commit

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import Axios from 'axios'
 
 const Instance = Axios.create({
-  baseURL: 'http://localhost:3000',
-  // baseURL: 'https://polar-mesa-44414.herokuapp.com',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://polar-mesa-44414.herokuapp.com',
   timeout: 60000
 })
 
